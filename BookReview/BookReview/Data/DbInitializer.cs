58a13587e67522865ceaa6a273ac292c77ed9307
Émile Nestor <76712426+emilenestor@users.noreply.github.com>
2021-06-15 15:05:57 +0200

    Updated SeedData

--- BookReview/BookReview/Data/DbInitializer.cs
+++ BookReview/BookReview/Data/DbInitializer.cs
@@ -10,16 +10,7 @@ namespace BookReview.Data
     {
         public static void Initialize(DbCon context)
         {
-            context.Database.EnsureCreated();
-
-            if (context.Book.Any())
-            {
-                return;   // DB has been seeded
-            }
-            else
-            {
-
-            }
+            context.Database.EnsureCreated();            
 
             if (context.Author.Any())
             {
@@ -28,8 +19,70 @@ namespace BookReview.Data
             else
             {
                 var authors = new Author[]
-                {   
-                new Author{FullName ="Paul Tannenberg", Birtdate = new DateTime(1968,08,02), DeathDate = null}
+                {
+                    new Author
+                    {
+                        FullName ="Paul Tannenberg", 
+                        Birtdate = new DateTime(1968,08,02), 
+                        DeathDate = null
+                    },
+                    new Author
+                    {
+                        FullName ="George Orwell", 
+                        Birtdate = new DateTime(1903,06,25), 
+                        DeathDate = new DateTime(1950,01,21), 
+                        Books = new List<Book>()
+                        {
+                            new Book
+                            {
+                                Title = "1984", 
+                                Description = "Winston Smith works for the Ministry of Truth in London, chief city of Airstrip One. Big Brother stares out from every poster, the Thought Police uncover every act of betrayal. When Winston finds love with Julia, he discovers that life does not have to be dull and deadening, and awakens to new possibilities.",
+                                ISBN = 9780141036144
+                            },
+                            new Book
+                            {
+                                Title = "Animal Farm", 
+                                Description = "Mr Jones of Manor Farm is so lazy and drunken that one day he forgets to feed his livestock. The ensuing rebellion under the leadership of the pigs Napoleon and Snowball leads to the animals taking over the farm. Vowing to eliminate the terrible inequities of the farmyard, the renamed Animal Farm is organised to benefit all who walk on four legs.", 
+                                ISBN = 9780141036137
+                            }
+                        }
+                    },
+                    new Author
+                    {
+                        FullName ="Agatha Christie", 
+                        Birtdate = new DateTime(1890,09,15), 
+                        DeathDate = new DateTime(1976,01,12), 
+                        Books = new List<Book>()
+                        {
+                            new Book
+                            {
+                                Title = "Cards on the Table", 
+                                Description = "A flamboyant party host is murdered in full view of a roomful of bridge players...", 
+                                ISBN = 9780008164898
+                            },
+                            new Book
+                            {
+                                Title = "Murder on the Orient Express", 
+                                Description = "Agatha Christie's most famous murder mystery, reissued with a striking new cover with special finishes.", 
+                                ISBN = 9780008226664
+                            }
+                        }
+                    },
+                    new Author
+                    {
+                        FullName ="Oscar Wilde",
+                        Birtdate = new DateTime(1854,10,16),
+                        DeathDate = new DateTime(1900,11,30),
+                        Books = new List<Book>()
+                        {
+                            new Book
+                            {
+                                Title = "The Picture of Dorian Gray",
+                                Description = "Enthralled by his own exquisite portrait, Dorian Gray exchanges his soul for eternal youth and beauty. Influenced by his friend Lord Henry Wotton, he is drawn into a corrupt double life; indulging his desires in secret while remaining a gentleman in the eyes of polite society. Only his portrait bears the traces of his decadence. The novel was a succes de scandale and the book was later used as evidence against Wilde at the Old Bailey in 1895. It has lost none of its power to fascinate and disturb.",
+                                ISBN = 9780141442464
+                            }
+                        }
+                    }
                 };
 
                 foreach (Author a in authors)
@@ -39,8 +92,6 @@ namespace BookReview.Data
 
                 context.SaveChanges();
             }
-
-            
         }
     }
 }
