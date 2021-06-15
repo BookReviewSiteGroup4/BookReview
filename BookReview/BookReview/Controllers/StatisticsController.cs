32b9c5d014ed1ea4dae2833d0996d61e5bfe6b40
Stefan Trenh <Stefantrenh@hotmail.com>
2021-06-15 16:43:15 +0200

    Best reviews and authors fixed

--- BookReview/BookReview/Controllers/StatisticsController.cs
+++ BookReview/BookReview/Controllers/StatisticsController.cs
@@ -26,10 +26,29 @@ namespace BookReview.Controllers
 
             svm.Author = author;
             svm.Book = book;
-
-      
-
             return View(svm);
+        }
+
+        public IActionResult TopAuthor()
+        {
+            var topAuthor = _context.Author.Include(a => a.Books).ThenInclude(e => e.Reviews).ToList();
+            var filterAuthor = topAuthor.Where(a => a.Books.Any(b => b.Reviews.Any()));
+            var getTopAuthors = filterAuthor.OrderByDescending(o => o.Books.Where(w => w.Reviews.Any()).Average(a => a.Reviews.Average(r => r.ReviewScore))).Take(3);
+
+            return View(getTopAuthors);
+        }
+
+        public IActionResult BestReviews()
+        {
+            var books = _context.Book.Include(a => a.Author).ThenInclude(x => x.Books).ThenInclude(w => w.Reviews).ToList();
+            var filterBooks = books.Where(a => a.Reviews.Any());
+            var filter3 = filterBooks.OrderByDescending(o => o.Reviews.Average(a => a.ReviewScore)).Take(3);
+
+            foreach (var item in filter3)
+            {
+                item.AverageRating = item.Reviews.Average(a => a.ReviewScore);
+            }
+            return View(filter3);
         }
     }
 }
